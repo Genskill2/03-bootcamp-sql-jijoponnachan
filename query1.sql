5dbac96c4b3cf911f8346c0a557e95cd855f1091
jijoponnachan <85068165+jijoponnachan@users.noreply.github.com>
2021-06-19 23:08:12 +0530

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,3 +1,4 @@
-### All books by PHI
-Creating a file called `query1.sql` which will contain a query that will print the names of all books by published by PHI.
+-- ### All books by PHI
+-- Creating a file called `query1.sql` which will contain a query that
+-- will print the names of all books by published by PHI.
 select b.title from books b, publisher p where b.publisher = p.id and p.name = "PHI";
