952342fc66ea4930a6466c96bf065eefee97fb2d
jijoponnachan <85068165+jijoponnachan@users.noreply.github.com>
2021-06-19 23:04:34 +0530

    Create update1.sql

---
+++ update1.sql
@@ -0,0 +1,4 @@
+-- ## Updating
+-- ### Change PHI to Prentice Hall
+-- Write a file called `update1.sql` which will contain a SQL statement to change the name of the -- publisher PHI to "Prentice Hall"
+update publisher set name = 'Prentice Hall' where name = 'PHI';
